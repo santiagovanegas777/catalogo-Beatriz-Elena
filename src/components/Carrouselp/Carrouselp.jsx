5244fb3fd130819b626188f21209e57760279a4d
correavanegassantiago777@gmail.com <correavanegassantiago777@gmail.com>
2024-05-05 17:33:36 +0200

    carrousel modificacion

--- src/components/Carrouselp/Carrouselp.jsx
+++ src/components/Carrouselp/Carrouselp.jsx
@@ -11,7 +11,19 @@ const Carrouselp = () => {
             <img src="https://res.cloudinary.com/dz3qrv6vz/image/upload/v1710075288/estilo/otgbcdl2pgvsejlo5huk.jpg" class="imgCarrousel" alt="gatos"/>
           </div>
           <div class="carousel-item">
-            <img src="https://res.cloudinary.com/dz3qrv6vz/image/upload/v1707666216/estilo/ww8izuvdmw3j0sqlturf.jpg" class="imgCarrousel" alt="prueba"/>
+            <img src="https://res.cloudinary.com/dz3qrv6vz/image/upload/v1714922562/estilo/qihx9bazn0owphvjortl.jpg" class="imgCarrousel" alt="prueba"/>
+          </div>
+          <div class="carousel-item">
+            <img src="https://res.cloudinary.com/dz3qrv6vz/image/upload/v1714922427/estilo/b7veequnlvgzqbeci0bj.jpg" class="imgCarrousel" alt="prueba"/>
+          </div>
+          <div class="carousel-item">
+            <img src="https://res.cloudinary.com/dz3qrv6vz/image/upload/v1714922473/estilo/agxsvagvs45vjpa2wvc5.jpg" class="imgCarrousel" alt="prueba"/>
+          </div>
+          <div class="carousel-item">
+            <img src="https://res.cloudinary.com/dz3qrv6vz/image/upload/v1714922532/estilo/nqy9o6kjsiiamoszxwj2.jpg" class="imgCarrousel" alt="prueba"/>
+          </div>
+          <div class="carousel-item">
+            <img src="https://res.cloudinary.com/dz3qrv6vz/image/upload/v1714922544/estilo/tesfyxo8ysuq08jsilga.jpg" class="imgCarrousel" alt="prueba"/>
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
